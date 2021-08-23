f84e79fa1f0780f3e5183f773c1a6addb63582d5
ConradPor <konradporoszewski@yahoo.com>
2021-08-23 19:23:56 +0200

    Code of Todo Application

---
+++ src/main/java/github/conpor/logic/ProjectService.java
@@ -0,0 +1,54 @@
+package github.conpor.logic;
+
+import github.conpor.TaskConfigurationProperties;
+import github.conpor.model.*;
+import github.conpor.model.projection.GroupReadModel;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class ProjectService {
+
+    private TaskGroupRepository taskGroupRepository;
+    private ProjectRepository repository;
+    private TaskConfigurationProperties config;
+
+    public ProjectService (final ProjectRepository repository,final TaskGroupRepository taskGroupRepository, final TaskConfigurationProperties config) {
+        this.repository = repository;
+        this.taskGroupRepository = taskGroupRepository;
+        this.config = config;
+    }
+
+
+    public List<Project> readAll() {
+        return repository.findAll();
+    }
+
+    public Project save(final Project toSave) {
+        return repository.save(toSave);
+    }
+
+    public GroupReadModel createGroup(LocalDateTime deadline, int projectId) {
+        if (!config.getTemplate().isAllowMultipleTasks() && taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
+            throw new IllegalStateException("Only one undone group from project is allowed!");
+        }
+        TaskGroup result = repository.findById(projectId)
+                .map(project -> {
+                    var rst = new TaskGroup();
+                    rst.setDescription(project.getDescription());
+                    rst.setTasks(
+                            project.getSteps().stream()
+                                    .map(projectStep -> new Task(
+                                            projectStep.getDescription(),
+                                            deadline.plusDays(projectStep.getDaysToDeadline()))
+                                    ).collect(Collectors.toSet())
+                    );
+                    rst.setProject(project);
+                    return taskGroupRepository.save(rst);
+                }).orElseThrow(() -> new IllegalArgumentException("Project with given id not found"));
+        return new GroupReadModel(result);
+
+    }
+}
