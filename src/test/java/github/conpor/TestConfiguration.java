b79ea1c997b474e1bee6b727ad1881915f7983ed
ConradPor <konradporoszewski@yahoo.com>
2021-08-24 12:45:07 +0200

    Add integration tests

--- src/test/java/github/conpor/TestConfiguration.java
+++ src/test/java/github/conpor/TestConfiguration.java
@@ -5,15 +5,30 @@ import github.conpor.model.Task;
 import github.conpor.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.util.*;
 
 @Configuration
 public class TestConfiguration {
+
     @Bean
+    @Primary
+    @Profile("!integration")
+    DataSource e2eTestDataSource() {
+        var result = new DriverManagerDataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa","");
+        result.setDriverClassName("org.h2.Driver");
+        return result;
+    }
+
+
+    @Bean
+    @Primary
     @Profile({"integration"})
     TaskRepository testRepo() {
         return new TaskRepository() {
@@ -51,7 +66,17 @@ public class TestConfiguration {
 
             @Override
             public Task save(final Task entity) {
-                return tasks.put(tasks.size() + 1, entity);
+
+                int key = tasks.size() + 1;
+                try {
+                    var field = Task.class.getDeclaredField("id");
+                    field.setAccessible(true);
+                    field.set(entity, key);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+                tasks.put(key, entity);
+                return tasks.get(key);
             }
         };
     }
